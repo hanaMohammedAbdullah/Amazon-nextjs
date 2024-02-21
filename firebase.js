c99380d93962a37df550225c993307cd128b0fec
Hana Mohammad Abdullah <73670213+hanaMohammedAbdullah@users.noreply.github.com>
2024-02-21 20:21:53 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjTGcJ4H13u2Pyr2ACFZFJceEC6pcf0hI",
-  authDomain: "e-nextjs-27943.firebaseapp.com",
-  projectId: "e-nextjs-27943",
-  storageBucket: "e-nextjs-27943.appspot.com",
-  messagingSenderId: "292303044116",
-  appId: "1:292303044116:web:03b6efea8c135bc349b101",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = !firebase.apps.length
